64023339b6be0a7d626f5fe2e257e7151eeb397a
koredeycode <koredey4u@gmail.com>
2023-04-17 13:07:47 +0000

    mandatory

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+export default function appendToEachArrayValue(array, appendString) {
+  for (var idx in array) {
+    var value = array[idx];
+    array[idx] = appendString + value;
+  }
+  for (let elem of array) {
+    elem = appendString + elem;
+  }
+
+  return array;
+}
---
+++ 0x00-ES6_basic/README.md
@@ -0,0 +1,3 @@
+# 0x00-ES6_basic
+In this project, I learned about ES6_basic as part of my projects for the ALX Software Engineering Programme
+* Koredeycode
