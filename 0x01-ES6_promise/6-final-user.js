ed9508169f330c8c7120e1941d3276d20de78af2
koredeycode <koredey4u@gmail.com>
2023-04-19 07:50:58 +0000

    done some tasks

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,10 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => (result.map((p) => ({
+      status: p.status,
+      value: p.status === 'fulfilled' ? p.value : String(p.reason),
+    }))));
+}
