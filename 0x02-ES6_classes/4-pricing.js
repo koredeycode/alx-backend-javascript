f7a3ece81c1e4fe83a85f5da3a5c39cbcf1f3576
koredeycode <koredey4u@gmail.com>
2023-04-20 15:34:02 +0000

    firsts

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,35 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor (amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  set amount (value) {
+    this._amount = value;
+  }
+
+  get amount () {
+    return this._amount;
+  }
+
+  set currency (value) {
+    if (!(value instanceof Currency)) {
+      throw new Error('currency should be a Currency');
+    }
+    this._currency = value;
+  }
+
+  get currency () {
+    return this._currency;
+  }
+
+  displayFullPrice () {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice (amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
---
+++ 0x02-ES6_classes/README.md
@@ -0,0 +1,3 @@
+# 0x02-ES6_classes
+In this project, I learned about ES6_classes as part of my projects for the ALX Software Engineering Programme
+* Koredeycode
