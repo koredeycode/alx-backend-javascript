37499d693338a7e6b6bb35fecc7e7d775d129911
koredeycode <koredey4u@gmail.com>
2023-04-20 17:41:20 +0000

    advanced

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,25 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+    return new Species();
+  }
+}
