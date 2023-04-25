ecf0fba7448feb46694df50082c14ad958d6ca12
koredeycode <koredey4u@gmail.com>
2023-04-25 00:06:23 +0000

    refactor

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,4 +1,4 @@
-export default function groceriesList() {
+/* export default function groceriesList() {
   return new Map([
     ['Apple', 10],
     ['Tomatoes', 10],
@@ -6,4 +6,14 @@ export default function groceriesList() {
     ['Rice', 1],
     ['Banana', 5],
   ]);
+} */
+export default function groceriesList() {
+  const values = [
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ];
+  return new Map(values);
 }
