5b22d18cf3b4139193c665af87ce8df254eb1286
koredeycode <koredey4u@gmail.com>
2023-08-23 16:24:46 +0000

    this

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Welcome to the payment system');
 });
@@ -10,7 +12,27 @@ app.get('/cart/:id(\\d+)', (req, res) => {
   res.send(`Payment methods for cart ${id}`);
 });
 
+app.get('/available_payments', (req, res) => {
+  const paymentMethods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.send(paymentMethods);
+});
+
+app.post('/login', (req, res) => {
+  let username = '';
+
+  if (req.body) {
+    username = req.body.userName;
+  }
+  res.send(`Welcome ${username}`);
+});
+
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
+
 module.exports = app;
