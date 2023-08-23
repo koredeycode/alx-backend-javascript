cbaea818f193cb09a65627d686cf38d4ee18a1b6
koredeycode <koredey4u@gmail.com>
2023-08-23 14:26:46 +0000

    this

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,9 @@
-#\!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ data: 'Successful response from API' });
+    }
+  });
+}
+
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,11 @@
-#\!/usr/bin/node
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', function () {
+  it('should get result of function', function (done) {
+    getPaymentTokenFromAPI(true).then((result) => {
+      expect(result).to.eql({ data: 'Successful response from the API' });
+      done();
+    });
+  });
+});
