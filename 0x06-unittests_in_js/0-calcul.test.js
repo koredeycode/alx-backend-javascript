1bada7b8318a1a0744c545dfaba18c723efaf4b4
koredeycode <koredey4u@gmail.com>
2023-08-23 11:29:10 +0000

    initialize repo

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,1 +1,42 @@
-#\!/usr/bin/node
+const calculateNumber = require('./0-calcul.js');
+const assert = require('assert');
+
+describe('calculateNumber', function () {
+  it('should return the sum of two rounded numbers', function () {
+    // Arrange
+    const a = 2.7;
+    const b = 3.4;
+
+    // Acts
+    const result = calculateNumber(a, b);
+
+    // Assert
+    assert.strictEqual(result, Math.round(a) + Math.round(b));
+  });
+
+  it('should return correct result for negative numbers', function () {
+    // Arrange
+    const a = -5.6;
+    const b = -3.1;
+
+    // Act
+    const result = calculateNumber(a, b);
+
+    // Assert
+    assert.strictEqual(result, Math.round(a) + Math.round(b));
+  });
+
+  it('should return correct result when one argument is zero', function () {
+    // Arrange
+    const a = 0;
+    const b = 8.9;
+
+    // Act
+    const result = calculateNumber(a, b);
+
+    // Assert
+    assert.strictEqual(result, Math.round(a) + Math.round(b));
+  });
+
+  // Add more test cases as needed
+});
