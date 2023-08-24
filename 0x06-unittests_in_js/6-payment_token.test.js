7df439cdc42a0bbdbb3f94e213f8cdb4f7c76756
koredeycode <koredey4u@gmail.com>
2023-08-24 09:38:12 +0100

    this

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -4,8 +4,8 @@ const { expect } = require('chai');
 describe('getPaymentTokenFromAPI', function () {
   it('should get result of function', function (done) {
     getPaymentTokenFromAPI(true).then((result) => {
-      expect(result).to.eql({ data: 'Successful response from the API' });
       done();
+      expect(result).to.eql({ data: 'Successful response from the API' });
     });
   });
 });
